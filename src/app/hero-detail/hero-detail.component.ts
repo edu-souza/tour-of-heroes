48e3b30ee3a4ab79776a8297295e8f371dda6d96
Eduardo de Souza <desouzanovesete@gmail.com>
2024-02-26 13:12:24 -0300

    saving tutorial progress...need to fix HeroesComponent import bug

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component,Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {NgIf} from '@angular/common';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [FormsModule,NgIf],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css'
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+}
