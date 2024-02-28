9dee0a34c8d8fafd04c77724d51c8d959db9e43b
Eduardo de Souza <desouzanovesete@gmail.com>
2024-02-28 14:05:01 -0300

    configuring InMemoryWebApiModule, fetching data from server

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig,importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import routeConfig from './routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { InMemoryDataService } from './in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routeConfig), 
     provideClientHydration(),
+    provideHttpClient(),
+    importProvidersFrom(InMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }))
   ]
 };
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
-
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
@@ -11,12 +11,44 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
 
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
   }
 
   getHero(id: number): Observable<Hero> {
