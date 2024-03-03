865ba7990d5dca8e2ccc8128965aeb1b5f359273
Eduardo de Souza <desouzanovesete@gmail.com>
2024-03-03 19:22:48 -0300

    add hero search component and, add,update and delete methods

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,11 +3,12 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,HeroSearchComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
